cb79c10ae26a742735fdced99057bdd8fa2f2998
Mamotechnolabs-zahid <zahid@mamotechnolabs.com>
2025-08-11 20:48:08 +0530

    Add Home component with layout structure and imports

---
+++ src/app/Version1/page.js
@@ -0,0 +1,25 @@
+import Header from "@/components/version1/Header";
+import Partners from "@/components/Partners";
+import Facility from "@/components/Facility";
+import Footer from "@/components/footer";
+import Insights from "@/components/Insight";
+import BourseSection from "@/components/BourseSection"
+import AboutSection from "@/components/AboutSection";
+import PillarOfProgress from "@/components/PillarOfProgress";
+
+
+export default function Home() {
+  return (
+        <div className="">
+            <Header/>
+            <Partners/>
+            <AboutSection/>
+            <PillarOfProgress/>
+            <Facility/>
+            <BourseSection/>
+            <Insights/>
+            <Footer/>
+        </div>
+
+  );
+}
